ba18f3a034395d9008f76b3ca91d031dafb90419
Octcarp <xproever@outlook.com>
2022-03-22 17:15:11 +0800

    Week6E3

---
+++ src/com/week6/E3.java
@@ -0,0 +1,19 @@
+package com.week6;
+
+import java.util.Scanner;
+
+public class E3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n= input.nextInt();
+        int[] threeTerms = {0,1,1};
+        for(int i=0;i<n;i++){
+            int b=threeTerms[1];
+            int c=threeTerms[2];
+            threeTerms[2]=b+c;
+            threeTerms[1]=c;
+            threeTerms[0]=b;
+        }
+        System.out.println(threeTerms[0]);
+    }
+}
