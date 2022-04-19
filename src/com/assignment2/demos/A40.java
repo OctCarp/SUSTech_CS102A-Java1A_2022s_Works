88c6129a6e6c11dee167ebac1ee99589da617fd6
Octcarp <xproever@outlook.com>
2022-04-19 16:59:19 +0800

    arrangement

--- src/com/assignment2/demos/A40.java
+++
@@ -1,7 +0,0 @@
-package com.assignment2.demos;
-
-public class A40 {
-    public static void main(String[] args) {
-        System.out.println("0");
-    }
-}
--- src/com/assignment2/demos/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.assignment2.demos;
-
-public class Demo {
-    public static void main(String[] args) {
-        String s="a2";
-        int i =s.charAt(1);
-        System.out.println(i);
-    }
-}
